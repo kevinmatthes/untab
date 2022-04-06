120f1ddf829e0bab65198a19b786d58a5610dc88
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-06 22:44:10 +0200

    Create dedicated header for GPL 2.0 macro

---
+++ GPL_2_0.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2022 Kevin Matthes
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * \author      Kevin Matthes
+ * \brief       .
+ * \copyright   (C) 2022 Kevin Matthes.
+ *              This file is licensed GPL 2 as of June 1991.
+ * \date        2022
+ * \file        .h
+ * \note        See `LICENSE' for full license.
+ *              See `README.md' for project details.
+ */
+
+/******************************************************************************/
+
+/*
+ * Security settings.
+ */
+
+#pragma once
+#ifndef ___H__
+#define ___H__
+
+
+
+/*
+ * Includes.
+ */
+
+// .
+
+
+
+/*
+ * Macros.
+ */
+
+// .
+
+
+
+/*
+ * Type declarations.
+ */
+
+// .
+
+
+
+/*
+ * Function declarations.
+ */
+
+// .
+
+
+
+/*
+ * Constants.
+ */
+
+// .
+
+
+
+/*
+ * End of header.
+ */
+
+// Tidying up.
+#ifndef ___INTERNAL__
+#endif  // ! ___INTERNAL__
+
+// Leaving the header.
+#endif  // ! ___H__
+
+/******************************************************************************/
